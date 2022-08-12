764295a62543e654feead1eb41d1fcd015fb3302
Renan Bernardo <renan.brnrd@gmail.com>
2022-08-12 09:06:49 -0300

    Add project files.

---
+++ PhonewordMSLearn/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PhonewordMSLearn;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PhonewordMSLearn/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PhonewordMSLearn;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
